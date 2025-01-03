03a6f86fac781e8681552e32cb7a52d2642bbe39
BugHunter-X <askzenai@gmail.com>
2025-01-03 23:48:04 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 10; // Correct index
+        System.out.println(arr[0]);
+    }
+}
